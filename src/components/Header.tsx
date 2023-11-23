527fe3f50c23292b368fe3dc7b88d2b852809152
mostafa <mirbabaei.mostafa@gmail.com>
2023-11-23 16:38:19 -0500

    update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import { useTranslation } from 'react-i18next';
-import './Header.css';
-import { Link, useNavigate } from 'react-router-dom';
-import detectBrowserLanguage from 'detect-browser-language';
-import langs from '../data/languages.json';
-import { useEffect, useState } from 'react';
+import { useTranslation } from "react-i18next";
+import "./Header.css";
+import { Link, useNavigate } from "react-router-dom";
+import detectBrowserLanguage from "detect-browser-language";
+import { BsSearch, BsCaretDownFill } from "react-icons/bs";
+import langs from "../data/languages.json";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const { t, i18n } = useTranslation();
+  const [langDisplay, setLangDisplay] = useState<string>("none");
   const [currentLang, setLang] = useState<string>(detectBrowserLanguage());
   const getFlag = () => {
     const cl = langs.filter((lang) => lang.type === currentLang);
@@ -27,19 +29,28 @@ const Header = () => {
   return (
     <div className="headerBox">
       <div className="headerLogo">
-        <span onClick={() => navigate('/')}>Online-Shopping</span>
+        <span onClick={() => navigate("/")}>Online-Shopping</span>
       </div>
       <div className="headerMenu">
         <div className="searchPannel">
-          <div className="searchPannelCategory">Alle</div>
+          <div className="searchPannelCategory">
+            {t("All")} <BsCaretDownFill />
+          </div>
           <div className="searchPannelInputBox">
-            <input />
+            <input className="searchPannelInput" placeholder={t("Search")} />
+          </div>
+          <div className="searchPannelSearchButton">
+            <BsSearch />
           </div>
         </div>
-        <div className="languageSelector">
-          <img src={currentFlag} className="languageSelectorFlag" />{' '}
+        <div
+          className="languageSelector"
+          onMouseOver={() => setLangDisplay("inline-block")}
+          onMouseOut={() => setLangDisplay("none")}
+        >
+          <img src={currentFlag} className="languageSelectorFlag" />{" "}
           <span>{currentLang}</span>
-          <div className="languageList">
+          <div className="languageList" style={{ display: langDisplay }}>
             {langs.map((lang, index) => {
               return (
                 <div
@@ -53,8 +64,8 @@ const Header = () => {
             })}
           </div>
         </div>
-        <div>
-          <Link to="/signin">{t('Signin')}</Link>
+        <div className="signinBox">
+          <Link to="/signin">{t("Signin")}</Link>
         </div>
       </div>
     </div>
